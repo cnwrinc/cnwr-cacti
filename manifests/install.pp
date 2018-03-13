a4334732db7d4d0bee6cc7f2fc15944e2ffe3eb5
Garrett Rowell <garrett2@cnwr.com>
2018-03-13 13:16:21 -0400

    dont move cacti source. stick with package defaults

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,13 +8,4 @@ class cacti::install inherits cacti{
     ensure => present,
   }
 
-  if($::selinux_enforced){
-
-    # Move http scripts under /var/www/html, add symlink to /usr/share/cacti and
-    # restore context on the files
-    exec {'mv /usr/share/cacti /var/www/html/ && ln -s /var/www/html/cacti /usr/share && restorecon -v -r /var/www/html/cacti':
-      creates => '/var/www/html/cacti',
-      require => Package[$::cacti_package];
-    }
-  }
 }
